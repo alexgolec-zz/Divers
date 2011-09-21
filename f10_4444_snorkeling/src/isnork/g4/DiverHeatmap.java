4d5f5e7e269861eb2c0f8fd6c4be5ace0103b468
Alexander K. Golec <akg2136@columbia.edu>
2011-09-20 20:36:53 -0400

    Changes to heatmap.

--- f10_4444_snorkeling/src/isnork/g4/DiverHeatmap.java
+++ f10_4444_snorkeling/src/isnork/g4/DiverHeatmap.java
@@ -2,15 +2,43 @@ package isnork.g4;
 
 import java.awt.Point;
 import java.awt.geom.Point2D;
-import java.util.HashSet;
+import java.util.Hashtable;
 
 import isnork.sim.Observation;
 import isnork.sim.SeaLifePrototype;
 
 public class DiverHeatmap {
 	private int dimension;
-	private double[][] danger;
-	private HashSet<StationaryCreature> stationaryCreatures;
+	private double[][] danger = null;
+	/** Mapping from id of observation to creature. */
+	private Hashtable<Integer, StationaryCreature> stationaryCreatures;
+	
+	/**
+	 * Utility function to perform fetching from the danger matrix.
+	 * @param x the x position
+	 * @param y the y position
+	 * @return the value of the danger matrix in this position
+	 */
+	public double dangerGet(int x, int y) {
+		refreshDanger();
+		
+		return dangerGetPrivate(x, y);
+	}
+
+	private double dangerGetPrivate(int x, int y) {
+		return danger[x + dimension][y + dimension];
+	}
+
+	
+	/**
+	 * Utility function to set values in the danger matrix
+	 * @param x the x position
+	 * @param y the y position
+	 * @param val the value to which to set the danger matrix entry
+	 */
+	private void dangerSet(int x, int y, double val) {
+		danger[x + dimension][y + dimension] = val;
+	}
 	
 	private static Point[] dangerRadius;
 	
@@ -44,38 +72,60 @@ public class DiverHeatmap {
 		}
 	}
 	
+	/**
+	 * Create a new empty heatmap
+	 * @param dimension the dimension parameter of the board
+	 */
 	public DiverHeatmap(int dimension) {
 		this.dimension = dimension;
-		stationaryCreatures = new HashSet<DiverHeatmap.StationaryCreature>();
+		stationaryCreatures = new Hashtable<Integer, StationaryCreature>();
 	}
 
-	public void tick() {
-		danger = new double[this.dimension][this.dimension];
-	}
-	
+	/**
+	 * Notify the heapmap of a direct observation of a stationary creature
+	 * @param o the observation to report
+	 */
 	public void registerStationary(Observation o) {
+		invalidateDanger();
+		
 		Point2D pos = o.getLocation();
-		stationaryCreatures.add(new StationaryCreature((int) pos.getX(), (int) pos.getY(), o));
+		stationaryCreatures.put(o.getId(), new StationaryCreature((int) pos.getX(), (int) pos.getY(), o));
 	}
 	
+	/**
+	 * Increment the cells around the stationary creature with its danger values
+	 * @param c the creature
+	 */
 	private void putStationaryCreature(StationaryCreature c) {
 		for (Point p: dangerRadius) {
+			if (!c.proto.isDangerous()) {
+				continue;
+			}
 			Point cur = new Point((int) c.pos.getX(), (int) c.pos.getY());
 			cur.setLocation(cur.x + p.x, cur.y + cur.y);
 			
 			try {
-				danger[cur.x][cur.y] += -2 * c.proto.getHappiness();
+				double old = dangerGetPrivate(cur.x, cur.y);
+				dangerSet(cur.x, cur.y, old - 2 * c.proto.getHappiness());
 			} catch (IndexOutOfBoundsException e) {
 				continue;
 			}
 		}
 	}
 	
-	public double[][] getDanger() {
-		for (StationaryCreature s: stationaryCreatures) {
-			putStationaryCreature(s);
+	private void refreshDanger() {
+		if (danger == null) {
+			danger = new double[dimension][dimension];
+		} else {
+			return;
 		}
 		
-		return danger;
+		for (Integer s: stationaryCreatures.keySet()) {
+			putStationaryCreature(stationaryCreatures.get(s));
+		}
+	}
+	
+	private void invalidateDanger() {
+		danger = null;
 	}
 }
