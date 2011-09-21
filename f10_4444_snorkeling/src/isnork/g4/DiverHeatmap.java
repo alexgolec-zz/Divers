b35f1adb28bf7438efc1cda7cfe7499a1aa8a72c
Alexander K. Golec <akg2136@columbia.edu>
2011-09-20 20:03:00 -0400

    Changed heatmap points to work on ints instead of doubles.

--- f10_4444_snorkeling/src/isnork/g4/DiverHeatmap.java
+++ f10_4444_snorkeling/src/isnork/g4/DiverHeatmap.java
@@ -1,5 +1,6 @@
 package isnork.g4;
 
+import java.awt.Point;
 import java.awt.geom.Point2D;
 import java.util.HashSet;
 
@@ -11,22 +12,22 @@ public class DiverHeatmap {
 	private double[][] danger;
 	private HashSet<StationaryCreature> stationaryCreatures;
 	
-	private static Point2D.Double[] dangerRadius;
+	private static Point[] dangerRadius;
 	
 	{
-		dangerRadius = new Point2D.Double[9];
+		dangerRadius = new Point[9];
 		
-		dangerRadius[0] = new Point2D.Double(0, 0);
-		dangerRadius[1] = new Point2D.Double(-1, 1);
-		dangerRadius[2] = new Point2D.Double(0, 1);
+		dangerRadius[0] = new Point(0, 0);
+		dangerRadius[1] = new Point(-1, 1);
+		dangerRadius[2] = new Point(0, 1);
 		
-		dangerRadius[3] = new Point2D.Double(1, 1);
-		dangerRadius[4] = new Point2D.Double(-1, 0);
-		dangerRadius[5] = new Point2D.Double(1, 0);
+		dangerRadius[3] = new Point(1, 1);
+		dangerRadius[4] = new Point(-1, 0);
+		dangerRadius[5] = new Point(1, 0);
 		
-		dangerRadius[6] = new Point2D.Double(-1, -1);
-		dangerRadius[7] = new Point2D.Double(0, -1);
-		dangerRadius[8] = new Point2D.Double(1, -1);
+		dangerRadius[6] = new Point(-1, -1);
+		dangerRadius[7] = new Point(0, -1);
+		dangerRadius[8] = new Point(1, -1);
 	}
 	
 	/**
@@ -34,11 +35,11 @@ public class DiverHeatmap {
 	 * @author alexgolec
 	 */
 	private class StationaryCreature {
-		public final Point2D.Double pos;
+		public final Point pos;
 		public final SeaLifePrototype proto;
 		
 		public StationaryCreature(int x, int y, Observation o) {
-			pos = new Point2D.Double(x, y);
+			pos = new Point(x, y);
 			proto = G4Diver.getProtoFromName(o.getName());
 		}
 	}
@@ -58,12 +59,12 @@ public class DiverHeatmap {
 	}
 	
 	private void putStationaryCreature(StationaryCreature c) {
-		for (Point2D.Double p: dangerRadius) {
-			Point2D.Double cur = new Point2D.Double(c.pos.getX(), c.pos.getY());
+		for (Point p: dangerRadius) {
+			Point cur = new Point((int) c.pos.getX(), (int) c.pos.getY());
 			cur.setLocation(cur.x + p.x, cur.y + cur.y);
 			
 			try {
-				danger[(int) cur.x][(int) cur.y] += -2 * c.proto.getHappiness();
+				danger[cur.x][cur.y] += -2 * c.proto.getHappiness();
 			} catch (IndexOutOfBoundsException e) {
 				continue;
 			}
